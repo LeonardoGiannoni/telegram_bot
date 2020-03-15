575c937cac95b6663f3b28a8848d6374602429dd
Leonardo_Giannoni <leonardo9622.lg@gmail.com>
2020-03-15 11:42:27 +0100

    csharp

--- Server.cs
+++ Server.cs
@@ -1,13 +1,10 @@
 using Nancy;
 
-namespace telegram {
-
-    class Server : NancyModule {
-        public Server() {
-            Get("/") = async (_, token) => 
-            {
-                return "Hello World";
-            };
+namespace telegram{
+    public class Server: NancyModule{
+        public Server(){
+            Get("/", _ => "ciao mondo!");
         }
     }
+
 }
