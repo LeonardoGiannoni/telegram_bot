b6195d0a61d227d1424e81ca1f4f5ec126c34f7e
Leonardo_Giannoni <leonardo9622.lg@gmail.com>
2020-03-15 17:58:03 +0100

    csharp

--- Program.cs
+++ Program.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using Telegram.Bot;
+﻿    using System;
+    using Telegram.Bot;
+    using Nancy.Hosting.Self;
+    using System.Threading.Tasks;
 
+//netsh http add urlacl url="http://+:12345/" user="Everyone"
 namespace telegram
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            //--client part
             var bot = new TelegramBotClient(Secrets.telegram_secret);
             var telegram_client = bot.GetMeAsync().Result;
             Console.WriteLine($"Ciao sono l'utente {telegram_client.Id} e il mio nome è {telegram_client.FirstName}");
-            Server server= new Server();
-            server.
+            
+            //--Server part--
+            string server_hostname = "localhost";
+            int port = 8081;
+            var config = new HostConfiguration()//prenota namespace dell'URL per utenti non admin 
+            {
+                UrlReservations = new UrlReservations() { CreateAutomatically = true }
+            };
+            var server_telegram  = new NancyHost(config, new Uri($"http://{server_hostname}:{port}"));
+            server_telegram.Start();
+            Console.ReadKey();
+            Console.WriteLine(String.Format($"Server started @ http://{server_hostname}:{port}"));
+            return 0;
         }
     }
 }
--- Server.cs
+++ Server.cs
@@ -1,10 +1,17 @@
 using Nancy;
+using System.Threading.Tasks;
+using Nancy.Extensions;
 
 namespace telegram{
     public class Server: NancyModule{
         public Server(){
             Get("/", _ => "ciao mondo!");
+
+            Post("/", _ =>
+            {
+                var jsonString = this.Request.Body.AsString();
+                
+            });
         }
     }
-
 }
