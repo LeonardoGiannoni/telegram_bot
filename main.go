095b95ae85737c32aee84aad66bd5d22adf58caf
Riccardo Berto <riccardobrt@gmail.com>
2020-03-16 19:11:19 +0100

    csharp

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"gopkg.in/macaron.v1"
 )
 
-type Json_Post struct {
+type postData struct {
 	Description string `json: "description"`
 	Type        string `json: "type"`
 	Time        string `json: "time"`
@@ -21,24 +21,24 @@ func handler(ctx *macaron.Context) string {
 }
 
 func main() {
+	go createBot()
 	//http.HandleFunc("/", handler)
-
 	m := macaron.Classic() // create a manager of web work application
 	fmt.Println("server is online")
 	m.Get("/prova", handler) //registering GET method
 
-	m.Post("/", binding.Json(Json_Post{}), func(jp Json_Post) string {
+	m.Post("/", binding.Json(postData{}), func(jp postData) string {
 		//return fmt.Sprintf("Auto: %s\nType: %s\nTime: %s\n",jp.Description, jp.Type, jp.Time)
 		//Type:modalità di allarme automatica o pre-impostata
 		//time orario trigger
 		//desription descrive cosa successo {user-text: "ciao", threshold_values: [minVal, Measured_Val, maxVal,]}
-		var json_Data []byte
-		json_Data, err := json.Marshal(jp)
+		var jsonData []byte
+		jsonData, err := json.Marshal(jp)
 		if err != nil {
 			log.Println(err)
 			return ""
 		}
-		return fmt.Println(string(json_Data))
+		return string(jsonData)
 
 	})
 	log.Println(http.ListenAndServe("0.0.0.0:8080", m))
--- telegram.go
+++ telegram.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"time"
 
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
-func CreateBot() {
-	b, _ := tb.NewBot(tb.Settings{
-		Token:  TelegramSecret,
+func createBot() {
+	b, err := tb.NewBot(tb.Settings{
+		Token: telegramSecret,
+		//URL:   "http://195.129.111.17:8012",
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 	})
+	b.Handle(tb.OnChannelPost, func(m *tb.Message) {
+		if m.FromChannel() {
+			fmt.Println(m.Chat.Type)
+			fmt.Println(m.Chat.Recipient())
+			b.Send(m.Chat, "test")
+		}
+	})
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Start()
 }
