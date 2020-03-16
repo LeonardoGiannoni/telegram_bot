ef8e8fb1f31a7cb6a0c57d39b7e101af2a16c472
Riccardo Berto <riccardobrt@gmail.com>
2020-03-16 17:02:30 +0100

    csharp

--- Server.cs
+++ Server.cs
@@ -3,23 +3,27 @@ using System.Threading.Tasks;
 using Nancy.Extensions;
 using Telegram.Bot.Types;
 
-namespace telegram{
-    public class Server: NancyModule{
-        public Server(){
-            Get("/", _ => {
-                
+namespace telegram
+{
+    public class Server : NancyModule
+    {
+        public Server()
+        {
+            Get("/", _ =>
+            {
+
                 return "ciao mondo!";
             });
 
             Post("/", _ =>
             {
                 var jsonString = this.Request.Body.AsString();
-                Message message= await botClient.SendTextMessageAsync(
+                Message message = await botClient.SendTextMessageAsync(
                 channelId: Secrets.telegram_channel,
                 text: "Test message to a Bot telegram",
                 parseMode: ParseMode.Markdown,
                 disableNotification: true,
-                replyToMessageId: e.Message.MessageId     
+                replyToMessageId: e.Message.MessageId
                 )
             });
         }
